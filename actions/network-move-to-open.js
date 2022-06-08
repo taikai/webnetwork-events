f228813eca1128ee8da4b9fde20aedc3d722d65a
MoshMage <moshmage@gmail.com>
2022-06-08 12:12:51 +0100

    scaffold the example action and add npm start script

--- actions/network-move-to-open.js
+++ actions/network-move-to-open.js
@@ -2,10 +2,10 @@ import {Network_v2} from "@taikai/dappkit";
 import {connection} from "../utils/connection.js";
 
 export const name = "move-bounties-to-open";
-export const schedule = "10 * * * * *";
+export const schedule = "1 * * * * *";
 export const description = "";
 export const author = "";
 
 export function action() {
-
+  console.log(`Hey!`)
 }
